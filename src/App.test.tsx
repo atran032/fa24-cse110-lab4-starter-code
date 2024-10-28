904d40746a43bdd5cbe8abd6e4fb00a65775213e
atran032 <ant032@ucsd.edu>
2024-10-28 02:55:30 -0700

    exercise 4 required done

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,17 +1,57 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, fireEvent, within, waitFor } from "@testing-library/react";
 import App from "./App";
 
 describe("Expense Creation", () => {
   test("create simple expense", () => {
     render(<App />);
 
-    const nameInput = screen.getByLabelText("Name");
-    fireEvent.change(nameInput, { target: { value: "new expense" } });
-    const costInput = screen.getByLabelText("Cost");
-    fireEvent.change(costInput, { target: { value: 100 } });
+    fireEvent.change(screen.getByLabelText("Name"), { target: { value: "new expense" } });
+    fireEvent.change(screen.getByLabelText("Cost"), { target: { value: 100 } });
     fireEvent.click(screen.getByText("Save"));
 
     expect(screen.getByText("new expense")).toBeInTheDocument();
     expect(screen.getByText("$100")).toBeInTheDocument();
   });
 });
+
+
+
+describe("Expense Deletion", () => {
+    test("delete simple expense", async () => {
+      render(<App />);
+  
+      fireEvent.change(screen.getByLabelText("Name"), { target: { value: "new expense" } });
+      fireEvent.change(screen.getByLabelText("Cost"), { target: { value: 100 } });
+      fireEvent.click(screen.getByText("Save"));
+
+      expect(screen.getByText("new expense")).toBeInTheDocument();
+      expect(screen.getByText("$100")).toBeInTheDocument();
+  
+      const expense = screen.getByTestId(`expense-0`);
+      fireEvent.click(within(expense).getByText("x"));
+      await waitFor(() => {
+        expect(screen.queryByText("new expense")).not.toBeInTheDocument();
+        expect(screen.queryByText("$100")).not.toBeInTheDocument();
+      });
+    });
+  });
+
+
+
+  describe("Budget Balance Verification", () => {
+    test("simple budget verification", () => {
+      render(<App />);
+
+      expect(screen.getByText("Budget: $1000")).toBeInTheDocument();
+      expect(screen.getByText("Remaining: $1000")).toBeInTheDocument();
+      expect(screen.getByText("Spent so far: $0")).toBeInTheDocument();
+
+      fireEvent.change(screen.getByLabelText("Name"), { target: { value: "new expense" } });
+      fireEvent.change(screen.getByLabelText("Cost"), { target: { value: 100 } });
+      fireEvent.click(screen.getByText("Save"));
+
+      expect(screen.getByText("Budget: $1000")).toBeInTheDocument();
+      expect(screen.getByText("Remaining: $900")).toBeInTheDocument();
+      expect(screen.getByText("Spent so far: $100")).toBeInTheDocument();
+    });
+  });
